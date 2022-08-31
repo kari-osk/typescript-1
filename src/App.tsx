5ea377b6ece22e117e06dddfe8f9fad68defe030
kari-osk <karinaosuka@gmail.com>
2022-08-31 11:00:33 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import './App.css';
+
+interface IPokemon {
+  id: number;
+  nome: string;
+  kilos?: number;
+  pokemon: boolean;
+  categoria: string;
+}
+
+const pokemons: IPokemon[] = [
+  {
+    id: 1,
+    nome: "bulbasaur",
+    kilos: 6.9,
+    pokemon: true,
+    categoria: "semente",
+  },
+  {
+    id: 2,
+    nome: "charmander",
+    kilos: 8.5,
+    pokemon: true,
+    categoria: "lagarto",
+  },
+  {
+    id: 3,
+    nome: "charizard",
+    kilos: 95.5,
+    pokemon: true,
+    categoria: "chama",
+  },
+  {
+    id: 4,
+    nome: "ash",
+    pokemon: false,
+    categoria: "treinador",
+  },
+
+]
+
+
+function App(props: IPokemon) {
+  return (
+    <div className="App">
+      <h1>Centro Pokemon</h1>
+      {
+        pokemons.map((pokemons) => {
+          return (
+            <div key={pokemons.id} className='card'>
+              <h3>Nome: {pokemons.nome}</h3>
+              <p>
+                {pokemons.pokemon ? 'É Pokemon: sim.' : 'É Pokemon: não.'}
+              </p>
+              <p>Categoria: {pokemons.categoria}</p>
+              <p>
+                {pokemons.kilos ? 'Peso: ' + pokemons.kilos + ' kg' : null}
+              </p>
+            </div>
+          )
+        })
+      }
+    </div>
+  );
+}
+
+export default App;
